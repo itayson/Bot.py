141630351890a314b60b9bef31b77ee5c23cf506
Tayson <taysonfuller2.0+github@gmail.com>
2025-03-21 20:26:35 -0300

    Atualizar o Gemini.py

--- Gemini.py
+++ Gemini.py
@@ -1,14 +1,9 @@
 import requests
-from telegram import Update, Bot
-from telegram.ext import Updater, CommandHandler, CallbackContext
 
-# Credenciais
-TELEGRAM_TOKEN = '7300611039:AAGXQ8JbRisjSNWNkjju9umvZ5BONO5p4Hs'
 GEMINI_API_URL = 'https://api.gemini.com/v1/pubticker/btcusd'  # Exemplo de URL da API
 GEMINI_API_KEY = 'AIzaSyCmFWODWWmR-TYceGn0x2CRKDJ4miK5d9E'
 
-# Função para buscar na API do Google Gemini
-def search_gemini():
+def get_gemini_data():
     headers = {
         'X-GEMINI-APIKEY': GEMINI_API_KEY,
     }
@@ -17,21 +12,3 @@ def search_gemini():
         return response.json()
     else:
         return None
-
-# Comando do Telegram para buscar na API do Google Gemini
-def search_command(update: Update, context: CallbackContext):
-    result = search_gemini()
-    if result:
-        update.message.reply_text(f"Resultado da busca: {result}")
-    else:
-        update.message.reply_text("Não foi possível obter resultados da API do Google Gemini.")
-
-def main():
-    updater = Updater(TELEGRAM_TOKEN, use_context=True)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler('search', search_command))
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
